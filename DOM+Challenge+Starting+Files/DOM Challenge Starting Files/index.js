c6df2b994be83dd774b98db40a89abc7055918e3
RUSHI DAULATKAR <rushidaulatkar@gmail.com>
2024-08-12 22:55:28 +0530

    DiceChallenge

--- DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,4 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Rushi";
+document.firstElementChild.lastElementChild.querySelector(".list").firstElementChild.style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.toggle("huge");
